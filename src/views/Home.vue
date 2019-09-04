dabbfff6c339e0cc96347c965bbda13e57717a5b
jhonatanmsc <jhonmscosta@gmail.com>
2019-09-04 09:24:14 -0300

    CHANGED: adaptation for admin mode

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,20 +1,57 @@
 <template>
-  <div class="home">
-    <Menu />
-    
+  <div id="home">
+
     <div class="container-fluid p-0">
+
+      <Intro />
+      <hr class="m-0">
+
+      <Exp />
+      <hr class="m-0">
+
+      <Edu />
+      <hr class="m-0">
+
+      <Skills />
+      <hr class="m-0">
+
+      <Interests />
+      <hr class="m-0">
+
     </div>
+    
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import Menu from '@/components/Menu'
+import Intro from '@/components/Intro'
+import Exp from '@/components/Exp'
+import Edu from '@/components/Edu'
+import Skills from '@/components/Skills'
+import Interests from '@/components/Interests'
+import { activeLi } from '@/assets/theme/js/resume.js'
+import 'firebase/database'
 
 export default {
   name: "home",
   components: {
-    Menu
+    Intro,
+    Exp,
+    Edu,
+    Skills,
+    Interests
+  },
+  data() {
+    return {
+      exp: '',
+      edu: '',
+      skills: ''
+    }
+  },
+  mounted: function() {
+    activeLi()
+  },
+  methods: {
   }
-};
+}
 </script>
