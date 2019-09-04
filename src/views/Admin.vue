351b60cb86c3518d3d079f5471511729a481d4dd
jhonatanmsc <jhonmscosta@gmail.com>
2019-09-04 09:22:43 -0300

    ADD: admin page

---
+++ src/views/Admin.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="admin">
+    <div class="container-fluid p-0">
+
+      <ExpEdit />
+      <hr class="m-0">
+
+      <EduEdit />
+      <hr class="m-0">
+
+      <SkillsEdit />
+      <hr class="m-0">
+
+    </div>
+  </div>
+</template>
+
+<script>
+import SkillsEdit from '@/components/SkillsEdit'
+import ExpEdit from '@/components/ExpEdit'
+import EduEdit from '@/components/EduEdit'
+import { activeLi } from '@/assets/theme/js/resume.js'
+
+export default {
+  name: 'admin',
+  components: {
+    SkillsEdit,
+    ExpEdit,
+    EduEdit
+  },
+  mount: function() {
+    activeLi()
+  }
+}
+</script>
