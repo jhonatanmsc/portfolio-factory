a062f5ed9e3a57f50307e8af6bf9f456a47e245d
jhonatanmsc <jhonmscosta@gmail.com>
2019-09-04 09:22:25 -0300

    CHANGED: prepare to global states

--- src/store.js
+++ src/store.js
@@ -1,10 +1,56 @@
 import Vue from "vue";
 import Vuex from "vuex";
+import { db } from '@/firebase'
 
 Vue.use(Vuex);
 
+
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {}
+  state: {
+    //   exp: [],
+    //   edu: [],
+    //   skills: []
+  },
+  getters: {
+        // getExp: state => {
+        //     return state.exp
+        // },
+        // getEdu: state => {
+        //     return state.edu
+        // },
+        // getSkills: state => {
+        //     return state.skills
+        // }
+  },
+  mutations: {
+    //   setExp (state, payload) {
+    //       state.exp = payload
+    //   },
+    //   setEdu (state, payload) {
+    //       state.edu = payload
+    //   },
+    //   setSkills (state, payload) {
+    //       state.skills = payload
+    //   }
+  },
+  actions: {
+    //   loadExp ({commit}) {
+    //     db.ref('exp')
+    //     .on('value', function(snapshot) {
+    //         commit('setExp', snapshot.val())
+    //     })
+    //   },
+    //   loadEdu ({commit}) {
+    //     db.ref('edu')
+    //     .on('value', function(snapshot) {
+    //         commit('setEdu', snapshot.val())
+    //     })
+    //   },
+    //   loadSkills ({commit}) {
+    //     db.ref('skills')
+    //     .on('value', function(snapshot) {
+    //         commit('setSkills', snapshot.val())
+    //     })
+    //   }
+  }
 });
