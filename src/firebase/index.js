3edd7328c45f4e5f057a249b74b63dd425d43006
jhonatanmsc <jhonmscosta@gmail.com>
2019-09-04 09:20:13 -0300

    ADD: firebase configuration file

---
+++ src/firebase/index.js
@@ -0,0 +1,30 @@
+import firebase from 'firebase/app'
+import 'firebase/auth'
+import 'firebase/storage'
+import 'firebase/database'
+
+let config = {
+    apiKey: process.env.VUE_APP_API_KEY,
+    authDomain: process.env.VUE_APP_AUTH_DOMAIN,
+    databaseURL: process.env.VUE_APP_DATABASE_URL,
+    projectId: process.env.VUE_APP_PROJECT_ID,
+    storageBucket: process.env.VUE_APP_STORAGE_BUTCKET,
+    messagingSenderId: process.env.VUE_APP_MESSAGING_SENDER_ID,
+    appId: process.env.VUE_APP_APP_ID
+};
+
+export const firebaseApp = firebase.initializeApp(config)
+export const db = firebaseApp.database()
+
+export default function install(Vue) {
+    Object.defineProperty(Vue.prototype, '$firebase', {
+        get () {
+            return firebaseApp
+        }
+    })
+    Object.defineProperty(Vue.prototype, '$db', {
+        get () {
+            return db
+        }
+    })
+} 
