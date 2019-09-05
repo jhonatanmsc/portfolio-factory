977659874f5593d9893aa273f94a210f581f3f1a
jhonatanmsc <jhonmscosta@gmail.com>
2019-09-05 15:25:32 -0300

    CHANGED: adaptado para funcionar corretamente com firebase

--- src/components/Skills.vue
+++ src/components/Skills.vue
@@ -3,44 +3,10 @@
         <section class="resume-section p-3 p-lg-5 d-flex align-items-center" id="skills">
             <div class="w-100">
             <h2 class="mb-5">Skills</h2>
-
             <div class="subheading mb-3">Limguagens de programação &amp; Ferramentas</div>
             <ul class="list-inline dev-icons">
-                <li class="list-inline-item">
-                <i class="fab fa-html5" title="HTML 5"></i>
-                </li>
-                <li class="list-inline-item">
-                <i class="fab fa-css3-alt" title="CSS 3"></i>
-                </li>
-                <li class="list-inline-item">
-                <i class="fab fa-js-square" title="Javascript"></i>
-                </li>
-                <li class="list-inline-item">
-                <i class="fab fa-python" title="Python"></i>
-                </li>
-                <li class="list-inline-item">
-                <i class="fab fa-node-js" title="Node"></i>
-                </li>
-                <li class="list-inline-item">
-                <i class="fab fa-php" title="PHP"></i>
-                </li>
-                <li class="list-inline-item">
-                <i class="fab fa-react" title="Reactjs"></i>
-                </li>
-                <li class="list-inline-item">
-                <i class="fab fa-linux" title="Linux"></i>
-                </li>
-                <li class="list-inline-item">
-                <i class="icon-docker" title="Docker"></i>
-                </li>
-                <li class="list-inline-item">
-                <i class="icon-postgres" title="PostgreSQL"></i>
-                </li>
-                <li class="list-inline-item">
-                <i class="icon-mysql" title="MySQL"></i>
-                </li>
-                <li class="list-inline-item">
-                <i class="icon-jquery" title="JQuery"></i>
+                <li v-for="item in items" :key="item.title" class="list-inline-item">
+                  <i :title="item.title" :class="item.icon" :style="'color: '+item.color" aria-hidden="true"></i>
                 </li>
             </ul>
 
@@ -64,41 +30,23 @@
     </div>
 </template>
 
+<script>
+export default {
+  data() {
+    return {
+      fields: ["title", "subtitle", "actions"],
+      items: []
+    }
+  },
+  mounted: function() {
+    let self = this
+    this.$db.ref('skills')
+      .on('value', function(snapshot) {
+          self.items = JSON.parse(snapshot.val().replace(/'/g,"\""))
+      })
+  },
+}
+</script>
+
 <style scoped>
-  .fa-html5:hover{
-    color: #e65100 !important;
-  }
-  .fa-css3-alt:hover{
-    color: #0288d1 !important;
-  }
-  .fa-js-square:hover{
-    color: #fdd835 !important;
-  }
-  .fa-react:hover{
-    color: #80deea !important;
-  }
-  .fa-node-js:hover{
-    color: #7cb342 !important;
-  }
-  .fa-python:hover{
-    color: #039be5 !important;
-  }
-  .icon-docker:hover{
-    color: #1a237e !important;
-  }
-  .fa-linux:hover{
-    color: #f9a825 !important;
-  }
-  .icon-postgres:hover{
-    color: #01579b !important;
-  }
-  .icon-mysql:hover{
-    color: #01579b !important;
-  }
-  .icon-jquery:hover{
-    color: #0d47a1 !important;
-  }
-  .fa-php:hover{
-    color: #5c6bc0 !important;
-  }
 </style>
