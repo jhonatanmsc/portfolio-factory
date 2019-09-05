ecf168f64aab37e5e3e7c52caf4b38d441cfa3df
jhonatanmsc <jhonmscosta@gmail.com>
2019-09-04 23:07:12 -0300

    ADD: vue the mask dependencie

--- src/main.js
+++ src/main.js
@@ -5,10 +5,12 @@ import store from "./store";
 import BootstrapVue from 'bootstrap-vue';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap-vue/dist/bootstrap-vue.css';
+import VueTheMask from 'vue-the-mask'
 import FirebaseVue from './firebase'
 
 Vue.use(BootstrapVue);
 Vue.use(FirebaseVue)
+Vue.use(VueTheMask)
 
 Vue.config.productionTip = false;
 
